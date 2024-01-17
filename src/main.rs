f11dcdf08c0d4dbbb9788d2b98b26f401395804a
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2024-01-17 13:46:29 -0600

    extract stuff into custom plugin

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(print_names)
-        .add_system(people_with_jobs)
-        .add_system(people_ready_for_hire)
-        .add_system(person_does_job)
+        .add_plugin(PeoplePlugin)
         .run()
+}
+
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system(print_names)
+            .add_system(people_with_jobs)
+            .add_system(people_ready_for_hire)
+            .add_system(person_does_job);
+    }
 }
 
 pub fn setup(mut commands: Commands) {
